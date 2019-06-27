4ade08432941276e2956c487872f3aa515848141
Eric Bodden <ericbodden30@gmail.com>
2019-06-27 01:37:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,23 +3,23 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  -1,-2,-3,-4
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  (-1,-2,-3,-4, dog)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  (-1,-2,-3,-4, cat)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  (-1,-2,-3,-4)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  #
 end
 
 def retrieve_element_from_index(array, index_number)
